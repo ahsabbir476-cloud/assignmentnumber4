3fde78a42fd713d8c7eeb039f722e639a0a9962b
ahsabbir476-cloud <ahsabbir476@gmail.com>
2026-02-22 17:29:45 +0600

    toggle effect added on  all, interview and rejected btn

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 let interviewList = [];
 let rejectedList = [];
+
 let totalNum = document.getElementById("totalboxcount");
 let interviewNum = document.getElementById("interviewboxcount");
 let rejectedNum = document.getElementById("rejectedboxcount");
 const allBoxParent  = document.getElementById("allbox");
+
+let btnAll = document.getElementById("btn-all")
+let btnInterview = document.getElementById("btn-interview")
+let btnRejected = document.getElementById("btn-rejected")
+
+
 function calculateCount(){
     totalNum.innerText = allBoxParent.children.length;
     interviewNum.innerText = interviewList.length;
     rejectedNum.innerText = rejectedList.length;
 }
 calculateCount()
+
+function toggleEffect(id) {
+    btnAll.classList.remove('bg-blue-500');
+    btnInterview.classList.remove('bg-blue-500');
+    btnRejected.classList.remove('bg-blue-500');
+
+    btnAll.classList.add('bg-white');
+    btnInterview.classList.add('bg-white');
+    btnRejected.classList.add('bg-white');
+
+    let selected = document.getElementById(id)
+    selected.classList.remove('bg-white')
+    selected.classList.add('bg-blue-500')
+}
